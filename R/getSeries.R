f05292cf55360a369a4d4958f31483528201fd71
Enrico Schumann <es@enricoschumann.net>
2019-02-13 16:23:41 +0100

    [getSeries] New arguments 'return.class', 'verbose' and 'dest.dir'

--- R/getSeries.R
+++ R/getSeries.R
@@ -1,5 +1,9 @@
 getSeries <- function(series, start = NULL,
-                      end = format(Sys.Date(), "%Y-%m")) {
+                      end = format(Sys.Date(), "%Y-%m"),
+                      return.class = NULL,
+                      verbose = TRUE,
+                      dest.dir = NULL) {
+
     on.exit(return(invisible(NULL)))
     if (grepl("^BBKRT", series)) {
         ## REAL TIME
@@ -10,16 +14,20 @@ getSeries <- function(series, start = NULL,
                       "&rtd_fileFormat=csv",
                       "&mode=rtd")
         con <- url(site)
-        message("Downloading data from Bundesbank ... ")
+        if (verbose)
+            message("Downloading data from Bundesbank ... ")
         txt <- try(readLines(con), silent = TRUE)
         em <- geterrmessage()
         close(con)
         if (inherits(txt, "try-error")) {
-            message("Failed.")
-            message("Error message: ", em)
+            if (verbose) {
+                message("Failed.")
+                message("Error message: ", em)
+            }
             return(invisible(NULL))
         } else
-            message("Done.")
+            if (verbose)
+                message("Done.")
 
         txt.head <- txt[1:5]
         txt.csv <- txt[-c(1:5)]
@@ -27,8 +35,8 @@ getSeries <- function(series, start = NULL,
                          header = FALSE,
                          sep = ",",
                          stringsAsFactors = FALSE)
-        row.names(tb) <- tb[[1]]
-        tb <- tb[, -1]
+        row.names(tb) <- tb[[1L]]
+        tb <- tb[, -1L]
 
         h.split <- strsplit(txt.head, " *, *")
         colnames(tb)                <- h.split[[1]][-1L]
@@ -42,6 +50,9 @@ getSeries <- function(series, start = NULL,
     }
 
     if (!is.null(start)) {
+        if (nchar(as.character(start)) == 4L)
+            start <- paste0(as.character(start), "-01")
+
         if (nchar(start) != 7L) {
             warning("'start' not in format YYYY-MM")
             tmp <- as.Date(as.character(start))
@@ -51,6 +62,9 @@ getSeries <- function(series, start = NULL,
         }
     }
     if (nchar(end) != 7L) {
+        if (nchar(as.character(end)) == 4L)
+            end <- paste0(as.character(end), "-12")
+
         warning("'end' not in format YYYY-MM")
         tmp <- as.Date(as.character(end))
         if (!is.na(tmp))
@@ -65,35 +79,54 @@ getSeries <- function(series, start = NULL,
                   "tsId=", series, "&mode=its&its_csvFormat=en",
                   "&its_currency=default&its_dateFormat=dateOfDay&",
                   sstart, "&", sto, sep = "")
-    con <- url(site)
-    message("Downloading data from Bundesbank ... ")
-    dats <- try(readLines(con), silent = TRUE)
-    em <- geterrmessage()
-    close(con)
-    if (inherits(dats, "try-error")) {
-        message("Failed.")
-        message("Error message: ", em)
-        return(NULL)
+    if (verbose)
+        message("Downloading data from Bundesbank ... ", appendLF = FALSE)
+    if (!is.null(dest.dir)) {
+        filename <- paste0(format(Sys.Date(), "%Y%m%d"),
+                           "__", series,
+                           "__", start,
+                           "__", end, ".csv")
+        filename <- file.path(dest.dir, filename)
+        if (!file.exists(filename))
+            download.file(site, filename, quiet = TRUE)
+        else
+            if (verbose)
+                message("use cache ... ", appendLF = FALSE)
+        dats <- try(readLines(filename), silent = TRUE)
+        em <- geterrmessage()        
     } else {
-        message("Done.")
+        con <- url(site)
+        dats <- try(readLines(con), silent = TRUE)
+        close(con)
+        em <- geterrmessage()
     }
-    dats <- read.csv(textConnection(dats), stringsAsFactors = FALSE)
+    dats <- read.csv(textConnection(dats),
+                     stringsAsFactors = FALSE)
+    if (inherits(dats, "try-error")) {
+        if (verbose) {
+            message("[Failed]")
+            message("Error message: ", em)
+        }
+        return(invisible(NULL))
+    } else
+        if (verbose)
+            message("[OK]")
+
     ## is last line a comment?
     if (dats[NROW(dats), 1L] == "") {
         doc <- dats[NROW(dats), 2L]
         dats <- dats[-NROW(dats), ]
-    } else {
+    } else
         doc <- NULL
-    }
 
-    doc0 <- dats[2:4,]
-    doc0 <- paste(doc0[ ,1L], doc0[ ,2L], sep = ": ")
+    doc0 <- dats[2:4, ]
+    doc0 <- paste(doc0[, 1L], doc0[, 2L], sep = ": ")
     doc0 <- c(dats[1L, 2L], doc0)
     doc <- c(doc0, doc)
     dats <- dats[-(1:4), ]
 
 
-    dates <- as.Date(dats[ ,1L])
+    dates <- as.Date(dats[, 1L])
     values <- dats[, 2L]
     NAs <- is.na(dates)
     dates <- dates[!NAs]
@@ -102,7 +135,21 @@ getSeries <- function(series, start = NULL,
     dates <- dates[!missing]
     values <- as.numeric(values[!missing])
     on.exit()
-    result <- list(dates = dates, values = values)
+
+    if (!is.null(return.class)) {
+        if (return.class == "zoo") {
+            if (requireNamespace("zoo"))
+                result <- zoo::zoo(values, dates)
+            else
+                stop("package ", sQuote("zoo"),
+                     " not available")
+
+        } else if (return.class == "data.frame")
+            result <- data.frame(dates = dates, values = values)
+
+    } else
+        result <- list(dates = dates, values = values)
+
     attr(result, "info") <-  doc
     result
 }
